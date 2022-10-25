c39f2567cdfcb4c5e9895c4f1b3c75bc6d078e26
adrisad <22082002andrademedina@gmail.com>
2022-10-25 14:25:00 -0400

    Agregamos el ejercicio 47

---
+++ 47/Ejercicio47.java
@@ -0,0 +1,20 @@
+
+public class Ejercicio47 {
+    public static String Chinos(int nivelReunion){
+        String res="";
+        if (esUnNumeroValido(nivelReunion)) {
+            if (nivelReunion==1) {
+                res="(-.-)";
+            }else{
+                res="(-."+Chinos(nivelReunion-1)+".-)";
+            }
+        }
+        return res;
+    }
+    private static boolean esUnNumeroValido(int number){
+        return number>0;
+    }
+    public static void main(String[] args) {
+        System.out.println(Chinos(1));
+    }
+}
