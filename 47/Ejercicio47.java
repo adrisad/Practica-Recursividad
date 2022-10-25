3eae032a28850d4ac4295f51877695d76d614d07
adrisad <22082002andrademedina@gmail.com>
2022-10-25 14:52:37 -0400

    Comentario

--- 47/Ejercicio47.java
+++ 47/Ejercicio47.java
@@ -14,6 +14,7 @@ public class Ejercicio47 {
     private static boolean esUnNumeroValido(int number){
         return number>0;
     }
+    // Compi
     public static void main(String[] args) {
         System.out.println(Chinos(1));
     }
