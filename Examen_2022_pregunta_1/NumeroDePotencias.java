5801f8f0990ca46f4b17485369bae59884d5a6db
adrisad <22082002andrademedina@gmail.com>
2022-10-26 14:48:38 -0400

    Modificando el examen

--- Examen_2022_pregunta_1/NumeroDePotencias.java
+++ Examen_2022_pregunta_1/NumeroDePotencias.java
@@ -1,7 +1,20 @@
 package Examen_2022_pregunta_1;
 
 public class NumeroDePotencias {
-    public int NumeroPotencias(int a,int b){
+    public int NumeroPotencias(int n,int b){
+        int res=0;
+        if (b<=n) {
+            if (n==0) {
+                return 0;
+            }else{
+                res=1+NumeroPotencias(n-, b)
+            }
+        }
         return 0;
+    }
+    private int GenerarNumero(int n,int b,int potenciaAux){
+        if () {
+            
+        }
     }
 }
